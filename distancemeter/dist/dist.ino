5ee1d4a60b077646f8a4bb2d5d80cd8fee388518
Ulam <m0ses@localhost.localdomain>
2019-12-21 20:53:50 +0100

    Modifing distancemeter sample code to register. Ready to paste into main.io

--- distancemeter/dist/dist.ino
+++ distancemeter/dist/dist.ino
@@ -12,9 +12,10 @@ long duration;
 int distance;
 
 void setup() {
-  
-pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
-pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+
+// Sets the trigPin as an Output and echoPin as an Input using registers
+DDRB = B00000010;
+PORTB = B00000000;
 
 Serial.begin(9600); // Starts the serial communication
 }
@@ -22,21 +23,21 @@ Serial.begin(9600); // Starts the serial communication
 void loop() {
   
 // Clears the trigPin
-digitalWrite(trigPin, LOW);
+PORTD = B00000000;
 delayMicroseconds(2);
 
 // Sets the trigPin on HIGH state for 10 micro seconds
-digitalWrite(trigPin, HIGH);
+PORTB = (1 << 1) | PORTD;
 delayMicroseconds(10);
-digitalWrite(trigPin, LOW);
+PORTB = ~(1 << 1) & PORTD;
 
 // Reads the echoPin, returns the sound wave travel time in microseconds
 duration = pulseIn(echoPin, HIGH);
 
 // Calculating the distance
 distance= duration*0.034/2;
-// Prints the distance on the Serial Monitor
 
+// Prints the distance on the Serial Monitor
 Serial.print("Distance: ");
 Serial.println(distance);
 }
