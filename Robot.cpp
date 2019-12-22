99814350b0db9ce9ed4d37fb58aa56a8aebf0370
Munio07 <39312548+Munio07@users.noreply.github.com>
2019-12-22 14:57:11 +0100

    Update Robot.cpp

--- Robot.cpp
+++ Robot.cpp
@@ -6,20 +6,27 @@
 
 int main()
 {
-    int a,b,c,d,e,f,g,h;
-    std::cin>>a;
-    std::cin>>b;
-    std::cin>>c;
-    std::cin>>d;
-    std::cin>>e;
-    std::cin>>f;
-    std::cin>>g;
-    std::cin>>h;
+    int mot1 = 1, mot2 = 2, mot3 = 3, mot4 = 4, mot5 = 5, mot6 = 6;
+    int axis_xl = 0, axis_yl = 0, axis_xr = 0;
 
-    DriveTrain drive_train(a, c, e, g);
-    drive_train.set_motors(b, d, f, h);
-    int recieve;
-    Communication com;
-    wiringPiISR(com.readPin, INT_EDGE_FALLING, &com.interrupt);
-    std::cout<<recieve;
+    Communication com();
+    DriveTrain drive_train(mot1, mot2, mot3, mot4, com);
+    Elevator elevator(mot5, com);
+    Intake intake(mot6, com);
+
+    int i = 0;
+
+    while(i<100)  {
+        std::cin>>axis_xl;
+        std::cin>>axis_yl;
+        std::cin>>axis_xr;
+
+        drive_train.drive(axis_yl, axis_xl, axis_xr);
+
+        int recieve;
+        wiringPiISR(com.readPin, INT_EDGE_FALLING, &com.interrupt);
+        std::cout<<recieve;
+
+        i++;
+    }
 }
