da87020ee6077dab63ab3f62862db9a12f12d72b
Munio07 <39312548+Munio07@users.noreply.github.com>
2019-12-22 14:57:53 +0100

    Update Elevator.h

--- Elevator.h
+++ Elevator.h
@@ -1,14 +1,18 @@
 #include <cstdint>
-
+#include "Communication.h"
 
 class Elevator  {
 
     private:
         uint8_t motor;
+        Communication com;
+        const int inputCorretion = 110;
 
     public:
         void set_motors(int16_t motor_value)  {
-
+            if(!com.setup(com.serial_port))  {
+                com.sendData(motor); com.sendData(motor_value + inputCorretion);
+            }
         }
 
         void move(int8_t direction)  {
@@ -24,7 +28,8 @@ class Elevator  {
             change_pos(0);
         }
 
-    Elevator(uint8_t mot)  {
+    Elevator(uint8_t mot, Communication comunication;)  {
         motor = mot;
+        com = communication;
     }
 };
