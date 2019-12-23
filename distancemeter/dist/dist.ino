2e617aa716b819973c8823a4168d7add798aaee3
Ulam <m0ses@localhost.localdomain>
2019-12-23 08:23:48 +0100

    I've pasted the distancemeter code, but there's some errors concerning vectors.

--- distancemeter/dist/dist.ino
+++ distancemeter/dist/dist.ino
@@ -22,22 +22,22 @@ Serial.begin(9600); // Starts the serial communication
 
 void loop() {
   
-// Clears the trigPin
-PORTD = B00000000;
-delayMicroseconds(2);
-
-// Sets the trigPin on HIGH state for 10 micro seconds
-PORTB = (1 << 1) | PORTD;
-delayMicroseconds(10);
-PORTB = ~(1 << 1) & PORTD;
-
-// Reads the echoPin, returns the sound wave travel time in microseconds
-duration = pulseIn(echoPin, HIGH);
-
-// Calculating the distance
-distance= duration*0.034/2;
-
-// Prints the distance on the Serial Monitor
-Serial.print("Distance: ");
-Serial.println(distance);
+  // Clears the trigPin
+  PORTD = B00000000;
+  delayMicroseconds(2);
+  
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  PORTB = (1 << 1) | PORTD;
+  delayMicroseconds(10);
+  PORTB = ~(1 << 1) & PORTD;
+  
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  
+  // Calculating the distance
+  distance= duration*0.034/2;
+  
+  // Prints the distance on the Serial Monitor
+  Serial.print("Distance: ");
+  Serial.println(distance);
 }
