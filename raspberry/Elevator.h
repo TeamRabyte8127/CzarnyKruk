3e290943cac65bf97049d73903f06fbd2a429f4b
adamowski137 <59111441+adamowski137@users.noreply.github.com>
2019-12-30 13:38:41 +0100

    Update Elevator.h

--- raspberry/Elevator.h
+++ raspberry/Elevator.h
@@ -4,23 +4,29 @@
 #define medium 20;
 #define high 30
 #define speedControl 20;
-#define marginError 3
+#define marginError 5
 
 class Elevator  {
 
     private:
+        uint8_t ElevatorMotor;
         uint8_t motor;
+        int8_t ElevatorMotor_value;
         int8_t motor_value;
         uint8_t target_position.; 
-        int8_t speed;
 
     public:
 
-        Elevator(uint8_t mot)  {
-            motor = mot;
+        Elevator(uint8_t mot, uint8_t mot2)  {
+            ElevatorMotor = mot;
+            motor = mot2;
         }
 
-        uint8_t getMotorValue()  {
+        int8_t getElevatorMotorValue()  {
+            return ElevatorMotor_value;
+        }
+        int8_t getMotorValue()
+        {
             return motor_value;
         }
 
@@ -39,15 +45,22 @@ class Elevator  {
                 break;
             }
         }
-        int8_t changePosition(uint8_t distance)  {
+        void changePosition(uint8_t distance)  {
             if (std::abs(distance-target_position>speedControl)
             {
-                motor_value = 50*(distance-target_position/(std::abs(distance-target_position)*(-1);
+                ElevatorMotor_value = 50*(distance-target_position/(std::abs(distance-target_position)*(-1);
             }
             else if (std::abs(distance-target_position<speedControl && std::abs(distance-target_position>marginError) 
             {
-                motor_value = 2,5*(std::abs(distance-target_position))*(distance-target_position/(std::abs(distance-target_position)*(-1);
+                ElevatorMotor_value = 2,5*(std::abs(distance-target_position))*(distance-target_position/(std::abs(distance-target_position)*(-1);
             }
         }
-    }
+        void getBall ()
+        {
+            motor_value = -100;
+        }
+        void shootBall()
+        {
+            motor_value = 100;
+        }
 };
